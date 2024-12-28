e3b6d4baefe31989c8e9c47846e3d8191e97bb62
animelegendpro <167015173+animelegendpro@users.noreply.github.com>
2024-12-29 00:55:17 +0530

    Update _config.php

--- _config.php
+++ _config.php
@@ -22,7 +22,7 @@ $disqus = "https://indianime.disqus.com"; // Disqus
 
 
 // API URL
-$api = "https://wordywelcomepostscript.shashankktiwari.repl.co"; // https://github.com/shashankktiwariii/anikatsu-api
+$api = "https://github.com/animelegendpro/aniwatch-api"; // https://github.com/shashankktiwariii/anikatsu-api
 
 
 $banner = $websiteUrl . "/files/images/banner.png";  //Banner
